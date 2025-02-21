67c16a746d29a25e5b3e21cc9a94b682b2539717
spagett-e <stefant.6127@gmail.com>
2025-02-21 09:44:41 +0800

    update babel.config.js

--- flowState/babel.config.js
+++ flowState/babel.config.js
@@ -1,20 +1,22 @@
 module.exports = function (api) {
-    api.cache(true)
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: [
-        [
-          '@tamagui/babel-plugin',
-          {
-            components: ['tamagui'],
-            config: './tamagui.config.ts',
-            logTimings: true,
-            disableExtraction: process.env.NODE_ENV === 'development',
-          },
-        ],
-  
-        // NOTE: this is only necessary if you are using reanimated for animations
-        'react-native-reanimated/plugin',
+  api.cache(true)
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel', // Add this line for Expo Router support
+
+      [
+        '@tamagui/babel-plugin',
+        {
+          components: ['tamagui'],
+          config: './tamagui.config.ts',
+          logTimings: true,
+          disableExtraction: process.env.NODE_ENV === 'development',
+        },
       ],
-    }
+
+      // NOTE: this is only necessary if you are using reanimated for animations
+      'react-native-reanimated/plugin',
+    ],
   }
+}
